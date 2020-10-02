dff479f7cbb5039cef8c642806a48ae0c67ddd17
fleximus <fleximus@users.noreply.github.com>
2020-10-03 01:58:37 +0200

    Start with kernel polling enabled and give a better output that the service started

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-erl -pa ebin deps/*/ebin -s redirect_service \
-	-eval "io:format(\"Point your browser at http://localhost:8080~n\")."
+
+cd `dirname $0`
+/usr/local/bin/erl $1 -noshell -pa ebin deps/*/ebin -s redirect_service \
+	-eval "io:format(\"Redirect service started~n\")." +K true
